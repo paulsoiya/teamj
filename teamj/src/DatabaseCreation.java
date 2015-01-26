bda99482ca12741b5b643b46900549c3c3014685
Taylor Scott <tdscott10@gmail.com>
2015-01-25 17:07:15 -0700

    Remove Readme.rtf

--- teamj/src/DatabaseCreation.java
+++ teamj/src/DatabaseCreation.java
@@ -5,20 +5,6 @@ import org.sqlite.SQLiteConfig;
 import java.sql.SQLException;
 
 public class DatabaseCreation {
-	
-	public static final String DB_URL = "jdbc:sqlite:professional.db";
-	public static final String DRIVER = "org.sqlite.JDBC";
-    
-//    public static Connection getConnection() throws ClassNotFoundException {
-//        Class.forName(DRIVER);
-//        Connection connection = null;
-//        try {
-//            SQLiteConfig config = new SQLiteConfig();
-//            config.enforceForeignKeys(true);
-//            connection = DriverManager.getConnection(DB_URL,config.toProperties());
-//        } catch (SQLException ex) {}
-//        return connection;
-//    }
     
 	/*
 	 * Create the professional database
@@ -82,9 +68,5 @@ public class DatabaseCreation {
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
 		}
-	}
-	
-	public static void main(String[] args) {
-		createProfessionalDatabase();
 	}
 }
