49bf25eca2d47844e08e4999c9954af4effb576a
Taylor Scott <tdscott10@gmail.com>
2015-02-24 16:07:30 -0700

    Package updates.

--- src/main/tests/ServerSetupTest.java
+++ src/main/tests/ServerSetupTest.java
@@ -1,6 +1,16 @@
+package main.tests;
+
+import static org.junit.Assert.*;
+import java.io.File;
+import org.junit.Test;
+
 public class ServerSetupTest {
 
    @Test
    public void DatabaseCreationTest() {
+       File proDB = new File("teamj/professional.db");
+       assertTrue(proDB.exists());
+       File indDB = new File("teamj/individual.db");
+       assertTrue(indDB.exists());
    } 
 }
--- src/main/view/User.java
+++ src/main/view/User.java
@@ -1,4 +1,4 @@
-package view;
+package main.view;
 
 public class User {
 
