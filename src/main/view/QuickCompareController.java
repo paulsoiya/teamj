00d73b88f4d0b203f16c4581a326c99e7708fbb4
psoiya <paul@econceptmedia.com>
2015-02-27 19:34:05 -0700

    PS02272015A connected pages to quick compare

---
+++ src/main/view/QuickCompareController.java
@@ -0,0 +1,64 @@
+/**
+ * Controller for the QuickCompare FXML page
+ * 
+ * @author Paul Soiya II (psoiya@asu.edu)
+ * @version Feb 27, 2015
+ */
+package main.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+
+public class QuickCompareController 
+				implements Initializable, ControlledScreen{
+
+	ScreensController controller;
+	
+	public QuickCompareController(){
+		controller = new ScreensController();
+	}
+	
+	@Override
+	public void setScreenParent(ScreensController screenPage) {
+		controller = screenPage;
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * Changes the current FXML page to the register.fxml
+	 * @param e
+	 */
+	@FXML
+	private void changeToRegister(ActionEvent e){
+		controller.setScreen(Main.registrationName);
+	}
+	
+  
+	/**
+	 * Changes the current FXML page to the login.fxml
+	 * @param e
+	 */
+	@FXML
+	private void changeToLogin(ActionEvent e){
+		controller.setScreen(Main.loginName);
+	}
+
+	/**
+	 * Changes the current FXML page to the quickCompare.fxml
+	 * @param e
+	 */
+	@FXML
+	private void changeToQuickCompare(ActionEvent e){
+		controller.setScreen(Main.quickCompareName);
+	}
+	
+}
