75c9ebe55507bfe28e707e77c09a487ead10d682
Taylor Scott <tdscott10@gmail.com>
2015-03-22 14:54:20 -0700

    Update Professional database for mysql.

--- src/model/DatabaseCreation.java
+++ src/model/DatabaseCreation.java
@@ -17,38 +17,38 @@ public class DatabaseCreation {
       Statement stmt = null;
 
       try {
-         Class.forName("org.sqlite.JDBC");
-         c = DriverManager.getConnection("jdbc:sqlite:professional.db");
-         c.createStatement().execute("PRAGMA foreign_keys = ON");
+         Class.forName("com.mysql.jdbc.Driver");
+          c = DriverManager.getConnection("jdbc:mysql://localhost:3306/professional?" +
+                                          "user=root&password=password");
          stmt = c.createStatement();
 
          String sql = "CREATE TABLE Team(" +
-            "TeamID         TEXT    PRIMARY KEY," +
-            "TeamName       TEXT," +
-            "Logo           TEXT," +
-            "PrimaryColor   TEXT," +
-            "SecondaryColor TEXT);";
+            "TeamID         VARCHAR(4)    PRIMARY KEY," +
+            "TeamName       VARCHAR(64)," +
+            "Logo           VARCHAR(512)," +
+            "PrimaryColor   VARCHAR(32)," +
+            "SecondaryColor VARCHAR(32));";
          stmt.executeUpdate(sql);
 
          sql = "CREATE TABLE Player(" +
-            "PlayerID       TEXT    PRIMARY KEY," +
-            "PlayerName     TEXT," +
-            "Team           TEXT," +
-            "Picture        TEXT," +
+            "PlayerID       VARCHAR(128)    PRIMARY KEY," +
+            "PlayerName     VARCHAR(128)," +
+            "Team           VARCHAR(4)," +
+            "Picture        VARCHAR(512)," +
             "Height         INT," +
             "Weight         INT," +
-            "BirthDate      TEXT," +
-            "College        TEXT," +
+            "BirthDate      VARCHAR(32)," +
+            "College        VARCHAR(64)," +
             "Number         INT," +
             "FOREIGN KEY(Team) REFERENCES Team(TeamID));";
          stmt.executeUpdate(sql);
 
          sql = "CREATE TABLE GameLog(" +
             "GameID         INT    PRIMARY KEY," +
-            "Date           TEXT," +
+            "Date           VARCHAR(32)," +
             "Team           INT," +
             "Opponent       INT," +
-            "Score          TEXT," +
+            "Score          VARCHAR(16)," +
             "FOREIGN KEY(Team) REFERENCES Team(TeamID)," +
             "FOREIGN KEY(Opponent) REFERENCES Team(TeamID));";
          stmt.executeUpdate(sql);
