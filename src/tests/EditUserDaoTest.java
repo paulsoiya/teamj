f11198c6eb880fed61705144dc8041d73141c1d6
bmf-icarus <jrabago@asu.edu>
2015-03-28 20:51:18 -0700

    adding test class for EditController

---
+++ src/tests/EditUserDaoTest.java
@@ -0,0 +1,63 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+import service.*;
+import model.User;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class EditUserDaoTest {
+	private UserDao dao;
+	private User user1;
+	private User user2;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		AbstractDaoFactory daoFact = DaoFactory.getDaoFactory();
+
+		this.dao = daoFact.getUserDao();
+		Date now = new Date();
+		LocalDate dob = now.toInstant().atZone(ZoneId.systemDefault())
+				.toLocalDate();
+		this.user1 = new User("testemail1@asu.edu", "password", "TestFname",
+				"TestLname", dob);
+		this.user2 = new User("testemail2@asu.edu", "password", "TestFname",
+				"TestLname", dob);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void createUserTest() {
+        assertTrue(dao.createUser(user1));
+        DaoFactory.CON_URL_IND = "jdbc:mysql://localhost:3306/individual?user=root&password=wrongpassword";
+        assertFalse(dao.createUser(user2));
+	}
+    
+    @Test
+    public void editUserAccount() {
+       
+    }
+    
+    
+
+}
