cdf5546a3c92b1244f3f0909efdd563d33601d7c
Zach Moore <zcmoore@asu.edu>
2015-04-16 20:56:34 -0700

    Add share with friend to quick compare

--- src/controller/QuickCompareScreenController.java
+++ src/controller/QuickCompareScreenController.java
@@ -7,8 +7,9 @@
 package controller;
 
 import static view.MainNavigator.LOGIN_FXML;
-import static view.MainNavigator.REG_FXML;
 import static view.MainNavigator.QCDISPLAY_VIDEO_FXML;
+import static view.MainNavigator.REG_FXML;
+import static view.MainNavigator.SHARE_STAT_FXML;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,6 +20,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import model.Compare;
 import model.ProInfo;
 import model.TeamInfo;
@@ -26,48 +28,52 @@ import service.CompareDao;
 import service.DaoFactory;
 import service.ProInfoDao;
 import session.UserSession;
+import view.Main;
 import view.MainNavigator;
 
 public class QuickCompareScreenController implements Initializable {
-    
-    @FXML
-    private Label nameTxt;
-    @FXML
-    private Label positionTxt;
-    @FXML
-    private Label numberTxt;
-    @FXML
-    private Label teamTxt;
-    @FXML
-    private Label dobTxt;
-    @FXML
-    private Label weightTxt;
-    @FXML
-    private Label heightTxt;
-    @FXML
-    private Label collegeTxt;
-    @FXML
-    private Label attemptsTxt;
-    @FXML
-    private Label yardsTxt;
-    @FXML
-    private Label touchdownTxt;
-    @FXML
-    private ImageView playerPicture;
-    @FXML
-    private ImageView teamPicture;
+	
+	@FXML
+	private GridPane quickCompGP;
+	
+	@FXML
+	private Label nameTxt;
+	@FXML
+	private Label positionTxt;
+	@FXML
+	private Label numberTxt;
+	@FXML
+	private Label teamTxt;
+	@FXML
+	private Label dobTxt;
+	@FXML
+	private Label weightTxt;
+	@FXML
+	private Label heightTxt;
+	@FXML
+	private Label collegeTxt;
+	@FXML
+	private Label attemptsTxt;
+	@FXML
+	private Label yardsTxt;
+	@FXML
+	private Label touchdownTxt;
+	@FXML
+	private ImageView playerPicture;
+	@FXML
+	private ImageView teamPicture;
 	
 	view.MainNavigator controller;
-    
-    private UserSession session = UserSession.getInstance();
-    
-    DaoFactory daoFact = (DaoFactory) DaoFactory.getDaoFactory();
-    ProInfoDao infoDao = daoFact.getProInfoDao();
-    CompareDao compareDao = daoFact.getCompareDao();
-    
-    ProInfo info = new ProInfo();
-    TeamInfo teamInfo = new TeamInfo();
-    Compare comp = new Compare();
+	
+	private UserSession session = UserSession.getInstance();
+	
+	DaoFactory daoFact = (DaoFactory) DaoFactory.getDaoFactory();
+	ProInfoDao infoDao = daoFact.getProInfoDao();
+	CompareDao compareDao = daoFact.getCompareDao();
+	
+	ProInfo info = new ProInfo();
+	TeamInfo teamInfo = new TeamInfo();
+	Compare comp = new Compare();
 	
 	public QuickCompareScreenController() {
 		controller = new view.MainNavigator();
@@ -75,26 +81,26 @@ public class QuickCompareScreenController implements Initializable {
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-        int statsId = session.getProStatsId();
-        
-        info = infoDao.findProInfo(statsId);
-        teamInfo = infoDao.findTeamInfo(info.getTeam());
-        comp = compareDao.inputStats(session.getGameId(), statsId);
-        nameTxt.setText(info.getName());
-        positionTxt.setText(info.getPosition());
-        numberTxt.setText(Integer.toString(info.getNumber()));
-        collegeTxt.setText(info.getCollege());
-        heightTxt.setText(Integer.toString(info.getHeight()));
-        weightTxt.setText(Integer.toString(info.getWeight()));
-        dobTxt.setText(info.getDOB());
-        teamTxt.setText(teamInfo.getTeamName());
-        Image img = new Image(info.getPicture());
-        playerPicture.setImage(img);
-        Image imgTeam = new Image(teamInfo.getTeamPicture());
-        teamPicture.setImage(imgTeam);
-        attemptsTxt.setText(Integer.toString(comp.getProAttempts()));
-        yardsTxt.setText(Integer.toString(comp.getProYards()));
-        touchdownTxt.setText(Integer.toString(comp.getProTouchdowns()));
+		int statsId = session.getProStatsId();
+		
+		info = infoDao.findProInfo(statsId);
+		teamInfo = infoDao.findTeamInfo(info.getTeam());
+		comp = compareDao.inputStats(session.getGameId(), statsId);
+		nameTxt.setText(info.getName());
+		positionTxt.setText(info.getPosition());
+		numberTxt.setText(Integer.toString(info.getNumber()));
+		collegeTxt.setText(info.getCollege());
+		heightTxt.setText(Integer.toString(info.getHeight()));
+		weightTxt.setText(Integer.toString(info.getWeight()));
+		dobTxt.setText(info.getDOB());
+		teamTxt.setText(teamInfo.getTeamName());
+		Image img = new Image(info.getPicture());
+		playerPicture.setImage(img);
+		Image imgTeam = new Image(teamInfo.getTeamPicture());
+		teamPicture.setImage(imgTeam);
+		attemptsTxt.setText(Integer.toString(comp.getProAttempts()));
+		yardsTxt.setText(Integer.toString(comp.getProYards()));
+		touchdownTxt.setText(Integer.toString(comp.getProTouchdowns()));
 	}
 	
 	/**
@@ -106,20 +112,28 @@ public class QuickCompareScreenController implements Initializable {
 	private void changeToLogin(ActionEvent e) {
 		MainNavigator.loadScreen(LOGIN_FXML);
 	}
-    
-    /**
-     * Changes the current FXML page to the register.fxml
-     *
-     * @param e
-     */
-    @FXML
-    private void changeToRegister(ActionEvent e) {
-        MainNavigator.loadScreen(REG_FXML);
-    }
-    
-    @FXML
-    public void changeToVideoDisplay(ActionEvent e) {
-        MainNavigator.loadScreen(QCDISPLAY_VIDEO_FXML);
-    }
+	
+	/**
+	 * Changes the current FXML page to the register.fxml
+	 *
+	 * @param e
+	 */
+	@FXML
+	private void changeToRegister(ActionEvent e) {
+		MainNavigator.loadScreen(REG_FXML);
+	}
+	
+	@FXML
+	public void changeToVideoDisplay(ActionEvent e) {
+		MainNavigator.loadScreen(QCDISPLAY_VIDEO_FXML);
+	}
+	
+	@FXML
+	public void changeToShareStat(ActionEvent e) {
+		System.out.println("ABOUT TO SHARE STAT");
+		String screenshotPath = Main.takeScreenshot(quickCompGP);
+		session.setScreenshotPath(screenshotPath);
+		MainNavigator.loadScreen(SHARE_STAT_FXML);
+	}
 	
 }
