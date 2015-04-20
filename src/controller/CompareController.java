51de8c8134e01d661d3bfb15ef9c05677e90c4eb
Zach Moore <zcmoore@asu.edu>
2015-04-19 18:32:35 -0700

    Linked Compare Screen to Recent Games
    Fixes #30

--- src/controller/CompareController.java
+++ src/controller/CompareController.java
@@ -10,6 +10,7 @@ import static view.MainNavigator.DISPLAY_VIDEO_FXML;
 import static view.MainNavigator.HOME_FXML;
 import static view.MainNavigator.SHARE_STAT_FXML;
 import static view.MainNavigator.GRAPH_FXML;
+import static view.MainNavigator.GAME_FXML;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -33,48 +34,48 @@ import view.Main;
 import view.MainNavigator;
 
 public class CompareController implements Initializable {
-    
-	@FXML 
+	
+	@FXML
 	private GridPane compGP;
-    @FXML
-    private Label nameTxt;
-    @FXML
-    private Label positionTxt;
-    @FXML
-    private Label numberTxt;
-    @FXML
-    private Label teamTxt;
-    @FXML
-    private Label dobTxt;
-    @FXML
-    private Label weightTxt;
-    @FXML
-    private Label heightTxt;
-    @FXML
-    private Label collegeTxt;
-    @FXML
-    private Label attemptsTxt;
-    @FXML
-    private Label yardsTxt;
-    @FXML
-    private Label touchdownTxt;
-    @FXML
-    private ImageView playerPicture;
-    @FXML
-    private ImageView teamPicture;
+	@FXML
+	private Label nameTxt;
+	@FXML
+	private Label positionTxt;
+	@FXML
+	private Label numberTxt;
+	@FXML
+	private Label teamTxt;
+	@FXML
+	private Label dobTxt;
+	@FXML
+	private Label weightTxt;
+	@FXML
+	private Label heightTxt;
+	@FXML
+	private Label collegeTxt;
+	@FXML
+	private Label attemptsTxt;
+	@FXML
+	private Label yardsTxt;
+	@FXML
+	private Label touchdownTxt;
+	@FXML
+	private ImageView playerPicture;
+	@FXML
+	private ImageView teamPicture;
 	
 	view.MainNavigator controller;
-    
-    private UserSession session = UserSession.getInstance();
-    
-    DaoFactory daoFact = (DaoFactory) DaoFactory.getDaoFactory();
-    ProInfoDao infoDao = daoFact.getProInfoDao();
-    CompareDao compareDao = daoFact.getCompareDao();
-    TeamDao teamDao = daoFact.getTeamDao();
-    
-    ProInfo info = new ProInfo();
-    TeamInfo teamInfo = new TeamInfo();
-    Compare comp = new Compare();
+	
+	private UserSession session = UserSession.getInstance();
+	
+	DaoFactory daoFact = (DaoFactory) DaoFactory.getDaoFactory();
+	ProInfoDao infoDao = daoFact.getProInfoDao();
+	CompareDao compareDao = daoFact.getCompareDao();
+	TeamDao teamDao = daoFact.getTeamDao();
+	
+	ProInfo info = new ProInfo();
+	TeamInfo teamInfo = new TeamInfo();
+	Compare comp = new Compare();
 	
 	public CompareController() {
 		controller = new view.MainNavigator();
@@ -82,26 +83,26 @@ public class CompareController implements Initializable {
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-        int statsId = compareDao.findStatsId(session.getGameId());
-
-        info = infoDao.findProInfo(statsId);
-        teamInfo = teamDao.findTeamInfo(info.getTeam());
-        comp = compareDao.inputStats(session.getGameId(), statsId);
-        nameTxt.setText(info.getName());
-        positionTxt.setText(info.getPosition());
-        numberTxt.setText(Integer.toString(info.getNumber()));
-        collegeTxt.setText(info.getCollege());
-        heightTxt.setText(Integer.toString(info.getHeight()));
-        weightTxt.setText(Integer.toString(info.getWeight()));
-        dobTxt.setText(info.getDOB());
-        teamTxt.setText(teamInfo.getTeamName());
-        Image img = new Image(info.getPicture());
-        playerPicture.setImage(img);
-        Image imgTeam = new Image(teamInfo.getTeamPicture());
-        teamPicture.setImage(imgTeam);
-        attemptsTxt.setText(Integer.toString(comp.getProAttempts()));
-        yardsTxt.setText(Integer.toString(comp.getProYards()));
-        touchdownTxt.setText(Integer.toString(comp.getProTouchdowns()));
+		int statsId = compareDao.findStatsId(session.getGameId());
+		
+		info = infoDao.findProInfo(statsId);
+		teamInfo = teamDao.findTeamInfo(info.getTeam());
+		comp = compareDao.inputStats(session.getGameId(), statsId);
+		nameTxt.setText(info.getName());
+		positionTxt.setText(info.getPosition());
+		numberTxt.setText(Integer.toString(info.getNumber()));
+		collegeTxt.setText(info.getCollege());
+		heightTxt.setText(Integer.toString(info.getHeight()));
+		weightTxt.setText(Integer.toString(info.getWeight()));
+		dobTxt.setText(info.getDOB());
+		teamTxt.setText(teamInfo.getTeamName());
+		Image img = new Image(info.getPicture());
+		playerPicture.setImage(img);
+		Image imgTeam = new Image(teamInfo.getTeamPicture());
+		teamPicture.setImage(imgTeam);
+		attemptsTxt.setText(Integer.toString(comp.getProAttempts()));
+		yardsTxt.setText(Integer.toString(comp.getProYards()));
+		touchdownTxt.setText(Integer.toString(comp.getProTouchdowns()));
 	}
 	
 	/**
@@ -114,6 +115,16 @@ public class CompareController implements Initializable {
 		MainNavigator.loadScreen(HOME_FXML);
 	}
 	
+	/**
+	 * Changes the current FXML page to the games.fxml
+	 * 
+	 * @param e
+	 */
+	@FXML
+	private void changeToRecentGames(ActionEvent e) {
+		MainNavigator.loadScreen(GAME_FXML);
+	}
+	
 	@FXML
 	public void changeToShareStat(ActionEvent e) {
 		System.out.println("ABOUT TO SHARE STAT");
@@ -121,14 +132,14 @@ public class CompareController implements Initializable {
 		session.setScreenshotPath(screenshotPath);
 		MainNavigator.loadScreen(SHARE_STAT_FXML);
 	}
-    
-    @FXML
-    public void changetoVideoDisplay(ActionEvent e) {
-        MainNavigator.loadScreen(DISPLAY_VIDEO_FXML);
-    }
-    
-    @FXML
-    public void changeToGraph(ActionEvent e) {
-    	MainNavigator.loadScreen(GRAPH_FXML);
-    }
+	
+	@FXML
+	public void changetoVideoDisplay(ActionEvent e) {
+		MainNavigator.loadScreen(DISPLAY_VIDEO_FXML);
+	}
+	
+	@FXML
+	public void changeToGraph(ActionEvent e) {
+		MainNavigator.loadScreen(GRAPH_FXML);
+	}
 }
